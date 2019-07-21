bbb822bad39fb448a30090f9d1c34ba1016fd786
Ethan <kunzou@Kuns-MacBook-Pro.local>
2019-07-21 16:31:04 +0100

    Add code

--- server/simple_server.cpp
+++ server/simple_server.cpp
@@ -13,10 +13,6 @@ using namespace exchange;
 #define STOCKTYPE_VALBZ StockType::VALBZ
 #define STOCKTYPE_VALE StockType::VALE
 
-std::string BOND = "BOND";
-std::string VALBZ = "VALBZ";
-std::string VALE = "VALE";
-
 typedef int price;
 typedef int size;
 typedef std::map<price, size> orders;
@@ -42,16 +38,19 @@ std::map<string, Market_data*> empty_record() {
 
 std::map<string, Market_data*> record = empty_record();
 
+std::mutex record_mutex;
+
 std::string to_string_symbol(StockType symbol) {
     return StockType_Name(symbol);
 }
 
 class ServerImpl final : public Exchange::Service {
-    void Hello(ServerContext* context, grpc::ServerWriter<ServerMessage>* writer) {
+    void Hello(grpc::ServerWriter<ServerMessage>* writer) {
         ServerMessage* hello_response;
         hello_response->set_t(ServerMessage::HELLO);
         writer->Write(*hello_response);
         
+        record_mutex.lock();
         for (auto it = record.begin(); it != record.end(); ++it) {
             ServerMessage* book_response;
             ServerMessage::Position* position;
@@ -69,14 +68,16 @@ class ServerImpl final : public Exchange::Service {
             book_response->set_t(ServerMessage_MessageType_BOOK);
             book_response->set_allocated_book(position);
             writer->Write(*book_response);
-        }   
+        }  
+        record_mutex.unlock(); 
     }
 
-    void AddOrder(ServerContext* context, const exchange::ClientMessage_Transaction transaction,
+    void AddOrder(const exchange::ClientMessage_Transaction transaction,
         grpc::ServerWriter<ServerMessage>* writer) {
         std::string symbol = to_string_symbol(transaction.symbol());
         int price = transaction.price();
         int size = transaction.size();
+        record_mutex.lock();
         if (transaction.dir() == Dir::BUY) {
             if (record[symbol]->buy.find(price) == record[symbol]->buy.end())
                 record[symbol]->buy[price] = size;
@@ -88,14 +89,15 @@ class ServerImpl final : public Exchange::Service {
             else
                 record[symbol]->sell[price] += size;
         }
+        record_mutex.unlock();
     }
 
-    Status Serve(ServerContext* context, const ClientMessage* query, 
+    Status Message(ServerContext* context, const ClientMessage* query, 
         grpc::ServerWriter<ServerMessage>* writer) {
         if (query->t() == ClientMessage_MessageType_HELLO)
-            Hello(context, writer);
+            Hello(writer);
         else if (query->t() == ClientMessage_MessageType_ADD_ORDER)
-            AddOrder(context, query->add_order(), writer);
+            AddOrder(query->add_order(), writer);
         return Status::OK;
     }
 };
